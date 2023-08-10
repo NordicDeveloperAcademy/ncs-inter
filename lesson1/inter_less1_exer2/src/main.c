527ee970e690dc54699abfc62f615b769b89d803
Ali Aljaani <academy@nordicsemi.no>
2023-08-10 13:12:55 +0200

    Lesson1 - Exercise 2  base code

--- lesson1/inter_less1_exer2/src/main.c
+++ lesson1/inter_less1_exer2/src/main.c
@@ -27,14 +27,10 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 /* STEP 4 - Define the data type of the FIFO items*/
-struct data_item_t {
-	void *fifo_reserved;
-	uint8_t  data[MAX_DATA_SIZE];
-	uint16_t len;
-};
+
 
 /* STEP 3 - Define the FIFO */
-K_FIFO_DEFINE(my_fifo);
+
 
 
 void timer0_handler(struct k_timer *dummy)
@@ -83,27 +79,7 @@ static void producer_func(void *unused1, void *unused2, void *unused3)
 	ARG_UNUSED(unused3);
     static uint32_t dataitem_count =0;
     /*STEP 5  - Complete the producer thread functionality  */
-    while (1) {
-        int bytes_written; 
-        /* Generate a random number between MIN_DATA_ITEMS & MAX_DATA_ITEMS to represent the number of data items to send 
-        every time the producer thread is scheduled */
-        uint32_t data_number = MIN_DATA_ITEMS + sys_rand32_get()%(MAX_DATA_ITEMS-MIN_DATA_ITEMS+1);
-        for (int i =0; i<=data_number;i++){
-            /* Create a data item to send */
-            struct data_item_t *buf= k_malloc(sizeof(struct data_item_t));
-            if (buf == NULL){
-            /* Unable to locate memory from the heap */
-                LOG_ERR("Enable to allocate memory");
-                return;
-            }
-                bytes_written = snprintf(buf->data,MAX_DATA_SIZE,"Data Seq. %u:\t%u",dataitem_count,sys_rand32_get());
-                buf->len = bytes_written; 
-                dataitem_count++;
-                k_fifo_put(&my_fifo,buf);
-        }
-        LOG_INF("Producer: Data Items Generated: %u",data_number);
-        k_msleep(PRODUCER_SLEEP_TIME_MS);
-    }
+
 }
 
 static void consumer_func(void *unused1, void *unused2, void *unused3)
@@ -112,12 +88,7 @@ static void consumer_func(void *unused1, void *unused2, void *unused3)
 	ARG_UNUSED(unused2);
 	ARG_UNUSED(unused3);
     /*STEP 6 - Complete the consumer thread functionality */
-    while (1) {
-        struct data_item_t *rec_item;
-        rec_item = k_fifo_get(&my_fifo, K_FOREVER);
-        LOG_INF("Consumer: %s\tSize: %u",rec_item->data,rec_item->len);
-        k_free(rec_item);
-    }
+
 }
 
  K_THREAD_DEFINE(producer, STACKSIZE, producer_func, NULL, NULL, NULL,
