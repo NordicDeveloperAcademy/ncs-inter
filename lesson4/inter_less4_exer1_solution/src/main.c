e90438722403d1fb4292ec2495639dbb3753c5e3
Sollie <heidi-irene.sollie@nordicsemi.no>
2024-01-15 11:21:54 +0100

    Clean up code and step comments

--- lesson4/inter_less4_exer1_solution/src/main.c
+++ lesson4/inter_less4_exer1_solution/src/main.c
@@ -7,16 +7,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-/* STEP 3 - Add required header files */
+/* STEP 1.2 - Include the header files for SPI, GPIO and devicetree */
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 
-#define DELAY 		100
-#define LEDDELAY 	3000
+LOG_MODULE_REGISTER(Lesson4_Exercise1, LOG_LEVEL_INF);
 
-//DEFINE THE CHIP-SELECT BAR PIN NUMBER FOR THE BOARD
+#define READ_DELAY 		100
+#define LEDDELAY 		3000
+
+// Define the chip-select bar (CSB) pin number for the board
 #ifdef CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP
 	#define CSB_PIN 25
 #endif
@@ -36,20 +38,23 @@
 	#define CSB_PIN 18
 #endif
 
-/* STEP 5 - Obtain the SPI Controller node and GPIO node for CS pin */
-static const struct device *spidev = DEVICE_DT_GET(DT_NODELABEL(spi1));
-const struct device *gpiodev =DEVICE_DT_GET(DT_NODELABEL(gpio0));
+// Define LED0 pin number
+#define LED0	13
 
-/* STEP 6 - Define and initialize a spi_config structure */
+/* STEP 3 - Obtain the SPI controller node and GPIO node for CS pin */
+static const struct device * spidev = DEVICE_DT_GET(DT_NODELABEL(spi1));
+const struct device * gpiodev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+
+/* STEP 4 - Define and initialize an spi_config structure */
 const struct spi_config bme_spi_config = {
 	.frequency = 125000U,
 	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
 	.slave = 0,
 };
 
-//Data structure to store BME280 data
+// Data structure to store BME280 data
 struct bme280_data {
-	/* Compensation parameters. */
+	/* Compensation parameters */
 	uint16_t dig_t1;
 	int16_t dig_t2;
 	int16_t dig_t3;
@@ -69,12 +74,12 @@ struct bme280_data {
 	int16_t dig_h5;
 	int8_t dig_h6;
 
-	/* Compensated values. */
+	/* Compensated values */
 	int32_t comp_temp;
 	uint32_t comp_press;
 	uint32_t comp_humidity;
 
-	/* Carryover between temperature and pressure/humidity compensation. */
+	/* Carryover between temperature and pressure/humidity compensation */
 	int32_t t_fine;
 
 	uint8_t chip_id;
@@ -89,32 +94,31 @@ static int bme_read_reg(uint8_t reg, uint8_t *data, int size)
 {
 	int err;
 
+	/* STEP 5.1 - Set the transmit and receive buffers */
 	uint8_t tx_buffer = reg;
-
-	/* STEP 7.1 - Set the transmit and receive buffers */
 	struct spi_buf tx_spi_buf = {.buf = (void *)&tx_buffer, .len = 1};
 	struct spi_buf_set tx_spi_buf_set = {.buffers = &tx_spi_buf, .count = 1};
 	struct spi_buf rx_spi_bufs = {.buf = data, .len = size};
 	struct spi_buf_set rx_spi_buffer_set = {.buffers = &rx_spi_bufs, .count = 1};
 
-	/* STEP 7.2 - Enable the sensor by activating Chip-select (put CSB into active state) */
+	/* STEP 5.2 - Enable the sensor by activating the chip-select signal */
 	gpio_pin_set(gpiodev, CSB_PIN, 1);
 
-	/* STEP 7.3 - Write the transmit buffers to sensor using spi_write() */
+	/* STEP 5.3 - Write the transmit buffers to the sensor */
 	err = spi_write(spidev, &bme_spi_config, &tx_spi_buf_set);
 	if (err < 0) {
-		printk("\nspi_write FAIL: Error %d\n", err);
+		LOG_ERR("spi_write() failed, err: %d", err);
 		return err;
 	}
 
-	/* STEP 7.4 - Read the data from sensor using spi_read() */
+	/* STEP 5.4 - Read the data from the sensor */
 	err = spi_read(spidev, &bme_spi_config, &rx_spi_buffer_set);
 	if (err < 0) {
-		printk("\nspi_read FAIL: Error %d\n", err);
+		LOG_ERR("spi_read() failed, err: %d", err);
 		return err;
 	}
 
-	/* STEP 7.5 - Disable the device by putting CSB into inactive state */
+	/* STEP 5.5 - Put the device into inactive mode by disabling the cnip-select signal */
 	gpio_pin_set(gpiodev, CSB_PIN, 0);
 
 	return 0;
@@ -128,16 +132,17 @@ static int bme_write_reg(uint8_t reg, uint8_t value)
 {
 	int err;
 
-	//Bit7 is 0 for the write command
+	/* STEP 6.1 - [] */
+	/* Bit7 is 0 for the write command */
 	uint8_t tx_buf[] = {(reg & 0x7F), value};
 
-	/* STEP 8 - Set tx-buffer, enable CS, do spi_write() and then disable CS */
+	/* STEP 6.2 - Write the data-byte to the sensor register */
 	struct spi_buf tx_spi_buf = {.buf = tx_buf, .len = sizeof(tx_buf)};
 	struct spi_buf_set tx_spi_buf_set = {.buffers = &tx_spi_buf, .count = 1};
 	gpio_pin_set(gpiodev, CSB_PIN, 1);
 	err = spi_write(spidev, &bme_spi_config, &tx_spi_buf_set);
 	if (err < 0) {
-		printk("\nspi_write FAIL: Error %d\n", err);
+		LOG_ERR("spi_write() failed, err %d", err);
 		return err;
 	}
 	gpio_pin_set(gpiodev, CSB_PIN, 0);
@@ -152,9 +157,9 @@ void bme_calibrationdata_read(void){
 	uint8_t values[2];
 	uint8_t regaddr;
 
-	/* STEP 9 - Go through the following code. We are using bme_read_reg function
-	and reading required number of bytes from respective register location
-	and setting data in proper order to form compensation parameter values*/
+	/* STEP 7 - Go through the following code. We are using bme_read_reg()
+	to read the required number of bytes from respective register locations
+	and setting the data in proper order to form the compensation parameter values */
 	regaddr = 0x88;
 	bme_read_reg(regaddr, values, 2);
 	bmedata.dig_t1 = ((uint16_t)values[1])<<8 | values[0];
@@ -227,13 +232,13 @@ void bme_calibrationdata_read(void){
 	bme_read_reg(regaddr, values, 1);
 	bmedata.dig_h6 = values[0];
 
-	printk("\nReading the parameters from sensor for temperature, pressure, and humidity");
-	printk("\nT1: %d\tT2: %d\tT3: %d", bmedata.dig_t1, bmedata.dig_t2, bmedata.dig_t3);
-	printk("\nP1: %d\tP2: %d\tP3: %d", bmedata.dig_p1, bmedata.dig_p2, bmedata.dig_p3);
-	printk("\nP4: %d\tP5: %d\tP5: %d", bmedata.dig_p4, bmedata.dig_p5, bmedata.dig_p6);
-	printk("\nP7: %d\tP8: %d\tP9: %d", bmedata.dig_p7, bmedata.dig_p8, bmedata.dig_p9);
-	printk("\nH1: %d\tH2: %d\tH3: %d", bmedata.dig_h1, bmedata.dig_h2, bmedata.dig_h3);
-	printk("\nH4: %d\tH5: %d\tH6: %d", bmedata.dig_h4, bmedata.dig_h5, bmedata.dig_h6);
+	LOG_INF("Reading the parameters from sensor for temperature, pressure, and humidity");
+	LOG_INF("T1: %d\tT2: %d\tT3: %d", bmedata.dig_t1, bmedata.dig_t2, bmedata.dig_t3);
+	LOG_INF("P1: %d\tP2: %d\tP3: %d", bmedata.dig_p1, bmedata.dig_p2, bmedata.dig_p3);
+	LOG_INF("P4: %d\tP5: %d\tP5: %d", bmedata.dig_p4, bmedata.dig_p5, bmedata.dig_p6);
+	LOG_INF("P7: %d\tP8: %d\tP9: %d", bmedata.dig_p7, bmedata.dig_p8, bmedata.dig_p9);
+	LOG_INF("H1: %d\tH2: %d\tH3: %d", bmedata.dig_h1, bmedata.dig_h2, bmedata.dig_h3);
+	LOG_INF("H4: %d\tH5: %d\tH6: %d\n", bmedata.dig_h4, bmedata.dig_h5, bmedata.dig_h6);
 }
 
 /// @brief The function to read and print different BME280 registers
@@ -247,14 +252,15 @@ int bme_print_regs(void)
 	uint8_t data;
 	int err;
 
-	/* STEP 10 - Go through the following code and see how we are using
-	bme_read_reg function to read and print different registers (1byte) */
-	//Register addresses to read from (see the data sheet)
+	/* STEP 8 - Go through the following code and see how we are using
+	bme_read_reg() to read and print different registers (1-byte) */
+
+	// Register addresses to read from (see the data sheet)
 	uint8_t reg_id = 0xD0;
 	uint8_t regs_morecalib[16];
 	uint8_t regs_more[12];
 	
-	//Set the register addresses
+	// Set the register addresses
 	regs_morecalib[0] = 0xE1;
 	for (uint8_t i=0; i<15; i++)
 		regs_morecalib[i+1] = regs_morecalib[i] + 1;
@@ -263,41 +269,53 @@ int bme_print_regs(void)
 	for (uint8_t i=0; i<11; i++)
 		regs_more[i+1] = regs_more[i] + 1;
 
-	//Read 1 byte data from each register and print
-	printk("\n\nReading all BME280 registers (one by one with delay=%dms):", DELAY);	
+	// Read 1 byte data from each register and print
+	LOG_INF("Reading all BME280 registers (one by one with delay = %d ms):", READ_DELAY);	
 	err = bme_read_reg(reg_id, &buf, size);
-	if(err < 0){return err;}
+	if (err < 0)
+	{
+		LOG_INF("Error in bme_read_reg(), err: %d", err);
+		return err;
+	}
 	data = buf;
 	bmedata.chip_id = data;
-	printk("\n\tReg[0x%02x]  =  0x%02x", reg_id, data);
-	k_msleep(DELAY);
+	LOG_INF("\tReg[0x%02x]  =  0x%02x", reg_id, data);
+	k_msleep(READ_DELAY);
 	
-	//Reading from more calibration regissters
-	for(uint8_t i=0; i<sizeof(regs_morecalib); i++)
+	// Reading from more calibration registers
+	for (uint8_t i = 0; i < sizeof(regs_morecalib); i++)
 	{
 		err = bme_read_reg(regs_morecalib[i], &buf, size);
-		if(err < 0){return err;}
+		if (err < 0)
+		{
+			LOG_INF("Error in bme_read_reg(), err: %d", err);
+			return err;
+		}
 		data = buf;
-		printk("\n\tReg[0x%02x]  =  0x%02x", regs_morecalib[i], data);
-		k_msleep(DELAY);
+		LOG_INF("\tReg[0x%02x]  =  0x%02x", regs_morecalib[i], data);
+		k_msleep(READ_DELAY);
 	}
 
-	//Reading from more registers
-	for(uint8_t i=0; i<sizeof(regs_more); i++)
+	// Reading from more registers
+	for (uint8_t i=0; i<sizeof(regs_more); i++)
 	{
 		err = bme_read_reg(regs_more[i], &buf, size);
-		if(err < 0){return err;}
+		if (err < 0)
+		{
+			LOG_INF("Error in bme_read_reg(), err: %d", err);
+			return err;
+		}
 		data = buf;
-		printk("\n\tReg[0x%02x]  =  0x%02x", regs_more[i], data);
-		k_msleep(DELAY);
+		LOG_INF("\tReg[0x%02x]  =  0x%02x", regs_more[i], data);
+		k_msleep(READ_DELAY);
 	}
 
 	return 0;
 }
 
-/* STEP 11 - Following compensation code taken from BME280 datasheet
-Just note their prototype and see that they use compensation parameters
-from the bme280_data struct and store back the compensated value */
+/* STEP 9 - Go through the compensation functions and 
+  note that they use the compensation parameters from 
+  the bme280_data structure and then store back the compensated value */
 static void bme280_compensate_temp(struct bme280_data *data, int32_t adc_temp)
 {
 	int32_t var1, var2;
@@ -324,7 +342,7 @@ static void bme280_compensate_press(struct bme280_data *data, int32_t adc_press)
 		((var1 * (int64_t)data->dig_p2) << 12);
 	var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)data->dig_p1) >> 33;
 
-	/* Avoid exception caused by division by zero. */
+	/* Avoid exception caused by division by zero */
 	if (var1 == 0) {
 		data->comp_press = 0U;
 		return;
@@ -357,60 +375,60 @@ static void bme280_compensate_humidity(struct bme280_data *data, int32_t adc_hum
 }
 
 /// @brief The function to read, compensate and show the
-///        Temperature, Pressure, and Humidity values from
+///        temperature, pressure, and humidity values from
 ///        the BME280 device
 int bme_read_values(void)
 {
-	//Set the registers addresses as per data-sheet
 
-	/* STEP 12.1 - Store register addresses to do burst read */
+	int err;
+
+	int32_t datap = 0, datat = 0, datah = 0;
+	float pressure = 0.0, temperature = 0.0, humidity = 0.0;
+
+	/* STEP 10.1 - Store register addresses to do burst read */
 	uint8_t regs[] = {0xF7, 0xF8, 0xF9, \
 					  0xFA, 0xFB, 0xFC, \
 					  0xFD, 0xFE, 0xFF};
-	
 	uint8_t readbuf[sizeof(regs)];
-	uint8_t size = sizeof(regs);
-	int32_t datap = 0, datat = 0, datah = 0;
-	float pressure=0.0, temperature=0.0, humidity=0.0;
-	int err;
 
-	/* STEP 12.2 - Set the transmit and receive buffers */
+	/* STEP 10.2 - Set the transmit and receive buffers */
 	struct spi_buf tx_spi_buf = {.buf = (void *)&regs, .len = sizeof(regs)};
 	struct spi_buf_set tx_spi_buf_set = {.buffers = &tx_spi_buf, .count = 1};
-	struct spi_buf rx_spi_bufs = {.buf = readbuf, .len = size};
+	struct spi_buf rx_spi_bufs = {.buf = readbuf, .len = sizeof(regs)};
 	struct spi_buf_set rx_spi_buffer_set = {.buffers = &rx_spi_bufs, .count = 1};
 
-	/* STEP 12.3 - Enable CS, do spi_transceive (trasmit+receive) and then disable CS */
+	/* STEP 10.3 - Use spi_transceive() to transmit and receive at the same time */
 	gpio_pin_set(gpiodev, CSB_PIN, 1);
 	err = spi_transceive(spidev, &bme_spi_config, &tx_spi_buf_set, &rx_spi_buffer_set);	
 	if (err < 0) {
-		printk("\nSPI TRANSCEIVE FAILED: Error %d\n", err);
+		LOG_ERR("spi_transceive() failed, err: %d", err);
 		return err;
 	}
 	gpio_pin_set(gpiodev, CSB_PIN, 0);
 
-	/* Put the data read from from registers into actual order (see datasheet) */
-	//uncompensated pressure value
+	/* Put the data read from registers into actual order (see datasheet) */
+	// Uncompensated pressure value
 	datap = (readbuf[1] << 12) | (readbuf[2] << 4) | ((readbuf[3] >> 4) & 0x0F);
-	//uncompensated temperature value
+	// Uncompensated temperature value
 	datat = (readbuf[4] << 12) | (readbuf[5] << 4) | ((readbuf[6] >> 4) & 0x0F);
-	//uncompensated humidity value
+	// Uncompensated humidity value
 	datah = (readbuf[7] << 8)  | (readbuf[8]);
 
-	//Compensate Pressure, Temperature and Humidity values using given functions	
+	// Compensate pressure, temperature and humidity values using given functions	
 	bme280_compensate_temp(&bmedata, datat);
 	bme280_compensate_press(&bmedata,datap);
 	bme280_compensate_humidity(&bmedata, datah);
 
-	//Convert to proper format as per data sheet
+	// Convert to proper format as per data sheet
 	temperature = (float)bmedata.comp_temp/100.0;
 	pressure 	= (float)bmedata.comp_press/256.0;
 	humidity 	= (float)bmedata.comp_humidity/1024.0;
 	
-	//Print the Uncompensated and Compensated values
-	printk("\nUncomp-Temp= %d\tComp-Temp= %fC", datat, temperature);
-	printk("\tUncomp-Pres= %d\tComp-Pres= %fPa", datap, pressure);
-	printk("\tUncomp-Hum= %d\tComp-Hum= %f%%RH", datah, humidity);
+	// Print the uncompensated and compensated values
+	LOG_INF("Temp \t uncomp = %d C \t comp = %f C", datat, temperature);
+	LOG_INF("Pressure \t uncomp = %d Pa \t comp = %f Pa", datap, pressure);
+	LOG_INF("Humidity \t uncomp = %d RH \t comp = %f%% RH", datah, humidity);
+
 	return 0;
 }
 
@@ -420,40 +438,42 @@ int bme_read_values(void)
 ///			and then data is read in while(1)
 int main(void)
 {
-	int ret;
+	int err;
 	
-	ret = device_is_ready(spidev);
-	if (!ret) {
-		printk("SPI DEV NOT Ready: Error %d\n", ret);
+	/* STEP 11.1 - Check if SPI and GPIO devices are ready */
+	err = device_is_ready(spidev);
+	if (!err) {
+		LOG_ERR("Error: SPI device is not ready, err: %d", err);
 		return 0;
 	}
 
-	ret = device_is_ready(gpiodev);
-	if (!ret) {
-		printk("GPIO DEV NOT Ready: Error %d\n", ret);
+	err = device_is_ready(gpiodev);
+	if (!err) {
+		LOG_ERR("Error: GPIO device is not ready, err: %d", err);
 		return 0;
 	}
 
-	//Configure CSB pin and LED0
+	/* STEP 11.2 - Configure the chip select pin and LED0 */
 	gpio_pin_configure(gpiodev, CSB_PIN, GPIO_OUTPUT | GPIO_ACTIVE_LOW);
-	gpio_pin_configure(gpiodev, 13, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure(gpiodev, LED0, GPIO_OUTPUT_ACTIVE);
 
-	//Read calibration data
+	/* STEP 11.3 - Read calibration data */
 	bme_calibrationdata_read();
 
-	//Write sampling parameters
+	/* STEP 11.4 - Write sampling parameters and read and print the registers */
 	bme_write_reg(0xF2, 0x04);
 	bme_write_reg(0xF4, 0x93);
 
-	//Read and print bme280 registers
 	bme_print_regs();
 
-	//Continuously read data values and toggle led	
-	printk("\n\nContinuously read sensor samples, compensate, and display (delay = %dms) ", LEDDELAY);
+	// Continuously read data values and toggle led	
+	LOG_INF("Continuously read sensor samples, compensate, and display (delay = %d ms) ", LEDDELAY);
 	while(1){
+		/* STEP 11.5 - Continously read sensor samples and toggle LED */
 		bme_read_values();
-		ret = gpio_pin_toggle(gpiodev, 13);
+		gpio_pin_toggle(gpiodev, LED0);
 		k_msleep(LEDDELAY);
 	}
+
 	return 0;
 }
