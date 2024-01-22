445e5eb24f3b4226a97fb1e2a46d6afd58ae8e71
runsiv <122086615+runsiv@users.noreply.github.com>
2024-01-22 14:44:35 +0100

    added exercise 1

---
+++ lesson2/inter_less2_exer1/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Starting exercise 1! %s\n", );
+	return 0;
+}
