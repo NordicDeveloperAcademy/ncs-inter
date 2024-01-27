29c70eb2fef25a8b4d6e640948dacd278e65bdcf
AliNordic <ali.aljaani@nordicsemi.no>
2024-01-27 16:00:42 +0100

    main() returns int

--- lesson8/inter_less8_exer1_solution/src/main.c
+++ lesson8/inter_less8_exer1_solution/src/main.c
@@ -21,23 +21,23 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-void main(void)
+int main(void)
 {
 	int ret;
 
 	if (!device_is_ready(led.port)) {
-		return;
+		return -1;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
-		return;
+		return -1;
 	}
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
-			return;
+			return -1;
 		}
 		k_msleep(SLEEP_TIME_MS);
 	}
