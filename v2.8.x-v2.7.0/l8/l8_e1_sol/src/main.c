51f770407dccd5e5099915a78db7de39eedcd0f1
Sigurd Hellesvik <sigurd.hellesvik@nordicsemi.no>
2025-01-21 09:53:01 +0100

    Added Exercise 1 for new lesson 8

---
+++ v2.8.x-v2.7.0/l8/l8_e1_sol/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+
+int main(){
+  printk("Hello from DevAcademy Intermediate, Lesson 8, Exercise 1\n");
+
+  return 0;
+}
