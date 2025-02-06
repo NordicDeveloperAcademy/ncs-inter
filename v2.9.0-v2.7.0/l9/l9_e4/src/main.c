26468dfd86eb4ee453b67668766b0e45cbcda2eb
Lukasz Bielak <lukasz.bielak@nordicsemi.no>
2025-02-06 11:50:00 +0100

    Update exercise 4

--- v2.9.0-v2.7.0/l9/l9_e4/src/main.c
+++ v2.9.0-v2.7.0/l9/l9_e4/src/main.c
@@ -1,22 +1,48 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-#include "cloud_connection.h"
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 
-#define CONNECTION_THREAD_STACK_SIZE 2048
+/* Step 5.4 - Include header for usb */
 
-/* Define, and automatically start the cloud connection thread. See cloud_connection.c */
-K_THREAD_DEFINE(con_thread, CONNECTION_THREAD_STACK_SIZE, cloud_connection_thread_fn,
-		NULL, NULL, NULL, 0, 0, 0);
+/* 1000 msec = 1 sec */
+
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+	int ret;
 
-  k_sleep(K_SECONDS(10));
-	printf("Hello World! %s\n", CONFIG_BOARD);
-	return 0;
+  /* Step 5.5 - Enable USB */
+
+	if (!device_is_ready(led.port)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+  return 1;
 }
