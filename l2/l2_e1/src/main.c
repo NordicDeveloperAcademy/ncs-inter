5e2e99c7d63924a958fb9aef69a6dd737a7eae89
Heidi Sollie <heidisollie@gmail.com>
2025-05-22 11:06:56 +0200

    Removed step 7

--- l2/l2_e1/src/main.c
+++ l2/l2_e1/src/main.c
@@ -7,18 +7,13 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-/* STEP 7.2.1 - Include the header file for the DK Buttons and LEDs library */
 
 LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 int8_t test_var = 124;
 
-/* STEP 7.2.2 - Define and initialize a callback handler for button presses */
-
-
 int main(void)
 {
-	/* STEP 7.2.3 - Initialize the DK Buttons and LEDs library */
-
+	
 	LOG_INF("Starting Exercise 1!");
 
 	/* STEP 4 - Add some logic to the application */
