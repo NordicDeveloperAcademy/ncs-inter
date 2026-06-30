83913e6a7feab58a109f65b9428c73af9f64f728
Lukasz Bielak <lukasz.bielak@nordicsemi.no>
2026-06-30 16:04:52 +0200

    Aligh base with the solution

--- l9/l9_e3_sol/src/main.c
+++ l9/l9_e3_sol/src/main.c
@@ -9,7 +9,7 @@
 
 /* 1000 msec = 1 sec */
 
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   100
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
