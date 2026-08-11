59ffe54642340a729f567f11c67c1bf406a66c2d
Lukasz Bielak <lukasz.bielak@nordicsemi.no>
2026-08-11 11:04:10 +0200

    Add nRF54LC10 flpr support in L8E2

--- l8/l8_e2_sol/sysbuild.cmake
+++ l8/l8_e2_sol/sysbuild.cmake
@@ -3,6 +3,8 @@ ExternalZephyrProject_Add(
   APPLICATION custom_image_name
   SOURCE_DIR ${APP_DIR}/../custom_image
   BOARD nrf54l15dk/nrf54l15/cpuflpr
-  # BOARD nrf5340dk/nrf5340/cpunet
   # BOARD nrf54lm20dk/nrf54lm20a/cpuflpr
+  # BOARD nrf54lc10dk/nrf54lc10a/cpuflpr
+  # BOARD nrf5340dk/nrf5340/cpunet
+
 )
